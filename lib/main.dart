3232ee15b4436619d541f3f3501cfb0d56962016
bhatiak2050 <karanbhatia2050@gmail.com>
2020-05-01 17:58:30 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(null),
+  ];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+    false, true, true,
+  ];
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[counter],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +74,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  if (answers[counter] == true) {
+                    scoreKeeper.add(Icon(Icons.check, color: Colors.green),);
+                  }
+                  else{
+                    scoreKeeper.add(Icon(Icons.close, color: Colors.red),);
+                  }
+                  counter++;
+                });
               },
             ),
           ),
@@ -79,12 +100,22 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  if (answers[counter] == false) {
+                    scoreKeeper.add(Icon(Icons.check, color: Colors.green),);
+                  }
+                  else{
+                    scoreKeeper.add(Icon(Icons.close, color: Colors.red),);
+                  }
+                  counter++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
